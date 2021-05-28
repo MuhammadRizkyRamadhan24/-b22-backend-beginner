fadfacb8cfa75655de3987f29281c89302f96afe
MuhammadRizkyRamadhan24 <zidan.muh69@gmail.com>
2021-05-28 20:28:54 +0700

    update syntax sql

--- src/models/items.js
+++ src/models/items.js
@@ -1,11 +1,11 @@
 const db = require('../helpers/db')
 
 exports.getItems = (cb) => {
-  db.query('SELECT * FROM items', cb)
+  db.query('SELECT items.id AS id ,name, image, categories.name_category AS category, variants.name_variant AS variant, detail, price FROM items INNER JOIN categories ON items.id_category = categories.id INNER JOIN variants ON items.id_variant = variants.id', cb)
 }
 
 exports.getItemById = (id, cb) => {
-  db.query('SELECT * FROM items WHERE id = ?', [id], cb)
+  db.query('SELECT items.id AS id ,name, image, categories.name_category AS category, variants.name_variant AS variant, detail, price FROM items INNER JOIN categories ON items.id_category = categories.id INNER JOIN variants ON items.id_variant = variants.id WHERE items.id = ?', [id], cb)
 }
 
 exports.createItem = (data, cb) => {
