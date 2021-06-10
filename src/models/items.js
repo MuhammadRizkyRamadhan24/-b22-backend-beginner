a3c03a3038c00bde5be5732bab1918ebc8e88f0e
MuhammadRizkyRamadhan24 <zidan.muh69@gmail.com>
2021-06-10 14:49:50 +0700

    add getItemByCategory & getItemDetail (models)

--- src/models/items.js
+++ src/models/items.js
@@ -14,7 +14,7 @@ exports.getSearch = (limit, page, sort, order, search, cb) => {
 }
 
 exports.createItem = (data, cb) => {
-  db.query('INSERT INTO items SET ? ', data, cb)
+  db.query('INSERT INTO `items`(`name`, `detail`, `price`, `delivery`, `quantity`, `image`) VALUES (?, ?, ?, ?, ?, ?)', [data.name, data.detail, data.price, data.delivery, data.quantity, data.image], cb)
 }
 
 exports.updateItem = (data, id, cb) => {
@@ -23,4 +23,12 @@ exports.updateItem = (data, id, cb) => {
 
 exports.deleteItem = (id, cb) => {
   db.query('DELETE FROM items WHERE id = ?', [id], cb)
+}
+
+exports.getItemByCategory = (id, cb) => {
+  db.query('SELECT items.id, items.name, items.image, items.price FROM items left JOIN items_categories on items_categories.id_items = items.id WHERE items_categories.id_category = ?', [id], cb)
+}
+
+exports.getItemDetail = (id, cb) => {
+  db.query('SELECT items.id, items.name, items.image, items.price AS base_price, items_variants.id AS id_variant, items_variants.additional_price, (items.price + items_variants.additional_price) AS end_price, items.delivery, items.detail, items.quantity, variants.name_variant AS variant, items.created_at, items.updated_at FROM items INNER JOIN items_variants ON items_variants.id_items = items.id INNER JOIN variants ON items_variants.id_variants = variants.id WHERE items.id = ?', [id], cb)
 }
