3542d7edd881ead33e1c799c486483c44986f53a
MuhammadRizkyRamadhan24 <zidan.muh69@gmail.com>
2021-06-10 14:50:11 +0700

    add route for getItemByCategories

--- src/routes/categories.js
+++ src/routes/categories.js
@@ -3,6 +3,7 @@ const categoryController = require('../controllers/categories')
 
 route.get('/', categoryController.getCategories)
 route.post('/', categoryController.createCategories)
+route.get('/:id/items', categoryController.getItemByCategory)
 route.get('/:id', categoryController.getDetailCategory)
 route.put('/:id', categoryController.updateCategories)
 route.delete('/:id', categoryController.deleteCategories)
