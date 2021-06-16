c037137264ac0fbd5b51f7cce29a816b43e1b018
MuhammadRizkyRamadhan24 <zidan.muh69@gmail.com>
2021-06-16 17:20:29 +0700

    add new route

--- index.js
+++ index.js
@@ -27,6 +27,8 @@ const variantRoute = require('./src/routes/variants')
 const itemsCategoriesRoute = require('./src/routes/itemsCategories')
 const itemsVariantsRoute = require('./src/routes/itemsVariants')
 const authRoute = require('./src/routes/auth')
+const privateRoute = require('./src/routes/private')
+const { auth } = require('./src/middlewares/auth')
 
 app.use('/static', express.static(path.join(__dirname, 'src/public')))
 app.use('/items', itemRoute)
@@ -35,6 +37,7 @@ app.use('/variant', variantRoute)
 app.use('/itemscategories', itemsCategoriesRoute)
 app.use('/itemsvariants', itemsVariantsRoute)
 app.use('/auth', authRoute)
+app.use('/private', auth, privateRoute)
 
 app.listen(process.env.APP_PORT, () => {
   console.log(`app running on port ${process.env.APP_PORT}`)
