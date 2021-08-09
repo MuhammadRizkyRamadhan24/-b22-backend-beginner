26283081ff1cdf35b6bf63c1bff337b462f7ea7f
MuhammadRizkyRamadhan24 <zidan.muh69@gmail.com>
2021-08-09 10:17:47 +0700

    update

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -7,17 +7,17 @@ const { validationResult } = require('express-validator')
 exports.register = async (req, res) => {
   const err = validationResult(req)
   if (!err.isEmpty()) {
-    if (err.errors.length > 1) {
-      const msg = []
-      err.errors.forEach(error => {
-        msg.push(
-          error.msg
-        )
-      })
-      return standardResponse(res, 401, false, msg)
-    } else {
-      return standardResponse(res, 401, false, err.errors[0].msg)
-    }
+    // if (err.errors.length > 1) {
+    //   const msg = []
+    //   err.errors.forEach(error => {
+    //     msg.push(
+    //       error.msg
+    //     )
+    //   })
+    //   return standardResponse(res, 401, false, msg)
+    // } else {
+    return standardResponse(res, 401, false, err.errors[0].msg)
+    // }
   } else {
     const data = req.body
     data.password = await bcrypt.hash(data.password, await bcrypt.genSalt())
@@ -41,17 +41,17 @@ exports.register = async (req, res) => {
 exports.login = async (req, res) => {
   const err = validationResult(req)
   if (!err.isEmpty()) {
-    if (err.errors.length > 1) {
-      const msg = []
-      err.errors.forEach(error => {
-        msg.push(
-          error.msg
-        )
-      })
-      return standardResponse(res, 401, false, msg)
-    } else {
-      return standardResponse(res, 401, false, err.errors[0].msg)
-    }
+    // if (err.errors.length > 1) {
+    //   const msg = []
+    //   err.errors.forEach(error => {
+    //     msg.push(
+    //       error.msg
+    //     )
+    //   })
+    //   return standardResponse(res, 401, false, msg)
+    // } else {
+    return standardResponse(res, 401, false, err.errors[0].msg)
+    // }
   } else {
     const { email, password } = req.body
     userModel.getUserByEmail(email, async (err, results) => {
